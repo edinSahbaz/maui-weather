353d0ae5adf09c0b7b2105462ad56bcb5d40de59
Edin Šahbaz <edin.sahbaz@hotmail.com>
2023-03-22 13:30:06 +0100

    Added Community Toolkit

--- WeatherApp/MauiProgram.cs
+++ WeatherApp/MauiProgram.cs
@@ -2,38 +2,30 @@
 using WeatherApp.Service;
 using WeatherApp.View;
 using WeatherApp.ViewModel;
+using CommunityToolkit.Maui;
 
 namespace WeatherApp;
-
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-				fonts.AddFont("FontAwesome-Regular-400.otf", "FaRegular");
-				fonts.AddFont("FontAwesome-Solid-900.otf", "FaSolid");
-            });
-
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder.UseMauiApp<App>().ConfigureFonts(fonts =>
+        {
+            fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            fonts.AddFont("FontAwesome-Regular-400.otf", "FaRegular");
+            fonts.AddFont("FontAwesome-Solid-900.otf", "FaSolid");
+        }).UseMauiCommunityToolkit();
 #if DEBUG
-		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
-
         builder.Services.AddSingleton<IConnectivity>(Connectivity.Current);
         builder.Services.AddSingleton<IGeolocation>(Geolocation.Default);
         builder.Services.AddSingleton<IMap>(Map.Default);
-
         builder.Services.AddSingleton<WeatherService>();
-		
         builder.Services.AddSingleton<HomePage>();
-		builder.Services.AddSingleton<HomeViewModel>();
-
+        builder.Services.AddSingleton<HomeViewModel>();
         return builder.Build();
-	}
+    }
 }
-
