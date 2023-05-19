78df7b48c530dcf4a0af99304cca5d952bf78618
Edin Šahbaz <edin.sahbaz@hotmail.com>
2023-05-19 15:47:10 +0200

    Settings Page start

---
+++ WeatherApp/View/SettingsPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WeatherApp.View;
+
+public partial class SettingsPage : ContentPage
+{
+	public SettingsPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ WeatherApp/ViewModel/SettingsViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace WeatherApp.ViewModel;
+
+public class SettingsViewModel : BaseViewModel
+{
+}
+
